467689baa993d1bb13fb6abc953293d75fb054f4
Taylor Ivanoff <taylor@orderup.com.au>
2021-04-01 17:27:49 +1100

    Improved styling.

--- run-folder.js
+++ run-folder.js
@@ -2,33 +2,46 @@ module.exports.requestGroupActions = [
   {
     label: 'Send All Requests',
     action: async (context, data) => {
+      console.log(context)
       const { requests } = data;
 
       let results = [];
+
       results.push(`<tr>
                       <th>Request</th>
                       <th>Status Code</th>
                       <th>Time</th>
                       <th>Bytes</th>
                     </tr>`);
+
       for (const request of requests) {
         const response = await context.network.sendRequest(request);
+
         var color = getStatusCodeColor(response.statusCode);
         var time = millisecToHumanReadable(response.elapsedTime);
+
         results.push(`<tr>
                         <td id="td_left">[${request.method}] ${request.name}</td>
-                        <td id="td_right"><font color="${color}">${response.statusCode} ${response.statusMessage}</font></td>
+                        <td id="td_left"><font color="${color}">${response.statusCode} ${response.statusMessage}</font></td>
                         <td id="td_right">${time}</td>
                         <td id="td_right">${response.bytesRead}</td>
                       </tr>`);
       }
 
-      const css = `table { margin: 0 auto 0 auto; }
-                   th, .label--small, label > small { text-align: left; padding: 10px 10px 10px 15px !important; padding-bottom: none !important; font-size: 15px !important; border: 1px solid #4B505C; }
-                   td { border: 1px solid #4B505C; color: #828282;}
-                   #td_left { text-align: left; padding: 3px 40px 3px 10px; }
-                   #td_right { text-align: center; padding: 3px 3px 3px 3px; }`
-      const html = `<html><head><style>${css}</style></head><body><table bgcolor="#282D35">${results.join('\n')}</table></body></html>`;
+      const css = `table { margin: 0 auto 0 auto; width: 100%; padding: 15px; }
+                   th, .label--small, label > small { text-align: left; padding: 15px !important; padding-bottom: none !important; font-size: 15px !important; }
+                   td { padding: 10px 10px 10px 15px; }
+                   #td_left { text-align: left; padding: 10px; }
+                   #td_right { text-align: center; padding: 10px; }`;
+                   
+      const html = `<html>
+                      <head>
+                        <style>${css}</style>
+                      </head>
+                      <body>
+                        <table>${results.join('\n')}</table>
+                      </body>
+                    </html>`;
 
       context.app.showGenericModalDialog('Results', { html });
     },
